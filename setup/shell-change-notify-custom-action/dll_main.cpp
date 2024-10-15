da0f51ab8a70ddd6ed8555b43f832631037d97ea
Victor Derks <vbaderks@gmail.com>
2024-10-15 23:24:25 +0200

    Link the custom action static with VC runtime lib + logging (#58)
    - Reduce dependencies by static linking with the VC runtime library.
    - Generate a log info message. This allows checking that the custom action was correctly called.

--- setup/shell-change-notify-custom-action/dll_main.cpp
+++ setup/shell-change-notify-custom-action/dll_main.cpp
@@ -5,9 +5,14 @@
 #include <msiquery.h>
 #include <ShlObj.h>
 
-unsigned int __stdcall SHChangeNotifyCustomAction(MSIHANDLE /*install*/)
+unsigned int __stdcall SHChangeNotifyCustomAction(const MSIHANDLE install)
 {
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, nullptr, nullptr);
+
+    // Create a log entry (allows checking if custom action was called correctly).
+    PMSIHANDLE record{MsiCreateRecord(1)};
+    MsiRecordSetString(record, 0, L"SHChangeNotify triggered");
+    MsiProcessMessage(install, INSTALLMESSAGE_INFO, record);
 
     return ERROR_SUCCESS;
 }
