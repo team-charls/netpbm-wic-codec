9dc8ace80fc823f6d2b9a3a6f8f6cdd325bbc83e
Victor Derks <vbaderks@gmail.com>
2024-12-19 00:07:18 +0100

    Update the Visual C++ 2015-2022 Redistributable runtime to v14.42.34433 (#65)
    v14.42.34433 is the runtime shipped with Visual Studio 2022 17.12. Projects build with 17.12 need at least this runtime.

--- create-signed-builds.cmd
+++ create-signed-builds.cmd
@@ -4,15 +4,15 @@
 :: Build signed x86
 msbuild -t:clean -p:Configuration=Release -p:Platform=x86
 msbuild -t:restore -p:Configuration=Release -p:Platform=x86
-msbuild -t:build -p:Configuration=Release -p:Platform=x86 -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
+msbuild -t:build -p:ContinuousIntegrationBuild=true -p:Configuration=Release -p:Platform=x86 -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
 
 :: Build signed x64
 msbuild -t:clean -p:Configuration=Release -p:Platform=x64
 msbuild -t:restore -p:Configuration=Release -p:Platform=x64
-msbuild -t:build -p:Configuration=Release -p:Platform=x64 -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
+msbuild -t:build -p:ContinuousIntegrationBuild=true -p:Configuration=Release -p:Platform=x64 -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
 
 :: Build signed ARM64
 msbuild -t:clean -p:Configuration=Release -p:Platform=ARM64
 msbuild -t:restore -p:Configuration=Release -p:Platform=ARM64
-msbuild -t:build -p:Configuration=Release -p:Platform=ARM64 -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
+msbuild -t:build -p:ContinuousIntegrationBuild=true -p:Configuration=Release -p:Platform=ARM64 -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
 
